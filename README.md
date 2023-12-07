0e293476ce3753f53c65c3fef3192d9d1c13bca1
Guillaume Gète <59868217+guillaumegete@users.noreply.github.com>
2023-12-07 19:41:27 +0100

    Moved project to Dialog.
    Updated version to use Dialog instead of IBM Notifier
    Moved from images to movies
    Localization added.

--- README.md
+++ README.md
@@ -1,18 +1,11 @@
 # Screen Recording PPPC Guide
 
-The Screen Recording PPPC Guide is a small script I wrote to guide the users to guide them to give screen recording to their favorite apps, using IBM Notifier (https://github.com/IBM/mac-ibm-notifications). It launches the Security and Privacy system preference in the background in the right pane, and displays a small how-to for the user to help them check the proper authorizations for screen recording.
-
-**IMPORTANT: it's not been adapted yet for macOS Ventura (the screen captures are not adapted for the new System Settings stuff), *however* it should work fine on it (I tested the URL to open the right prefpane and it works fine, you'll have to adapt the screen captures of course).**
+The Screen Recording PPPC Guide is a small script I wrote to guide the users to guide them to give screen recording to their favorite apps, using Swift Dialog (https://github.com/swiftDialog/swiftDialog). It launches the Security and Privacy system preference for macOS 12 and below, or system setting for macOS Ventura and higher, in the background in the right pane, and displays a small "how-to" for the user to help them check the proper authorizations for screen recording.
 
 ## How to use this?
 
 - Add the contents of the ScreenRecordingPPPCGuide.sh in a Jamf Pro script.
-- Create a smart group with criteria "Has IBM Notifier installed".
-- Create a Jamf Pro policy with the *At login* trigger, frequency set on "Once per computer per user", and scoped only on Macs that have IBM Notifier installed.
-- To keep control of deployment, I use DEPNotify with the [DEPNotify Starter](https://github.com/jamf/DEPNotify-Starter) script, installing IBM Notifier in the process.
-- At the end of deployment, perform `jamf recon` and request a reboot.
-- Computer reboots.
+- Create a Jamf Pro policy with the *At login* trigger, frequency set on "Once per computer per user".
 
-After reboot, the policy runs the Screen Recording PPPC Guide script, that launches IBM Notifier and displays a few dialogs with pictures. Adapt the script with the path and with your own dialog. If you use French, you can use the screen captures I added, otherwise, feel free to replace them with your own.
-
+Right after the user logs in, the policy is run and the Dialog app will display a few dialogs with videos. Adapt the script with the path and with your own dialog. If you use French, you can use the videos I added, otherwise, feel free to replace them with your own.
 
--- screenrecordingpppcguide.sh
+++ screenrecordingpppcguide.sh
@@ -8,73 +8,187 @@
 
 # v 1.0
 
-# Get current user
 
-consoleuser=$(stat -f%Su /dev/console)
+# v 1.0 : Initial release
+# v 1.1 : Adapted for macOS Ventura (non public)
+# v 1.2 : Rewritten to use SwitftDialog (and installs it if needed). Now uses videos instead of fixed images.
 
-# Ouverture du panneau de préférence
+# Variables
 
-sudo -u "$consoleuser" open "x-apple.systempreferences:com.apple.preference.security?Privacy_ScreenCapture"
+# Username
 
-# IBM Notifier binary path. Adapt to your own if you customized the IBM Notifier app. 
-# Note that you must specify the full path to the binary inside the .app file!
-# i.e. /Applications/Utilities/IBM Notifications.app/Contents/MacOS/IBM Notifications
-# Do not escape spaces in the path.
+consoleUser=$(stat -f%Su /dev/console)
 
-NA_PATH="/Applications/Utilities/IBM Notifications.app/Contents/MacOS/IBM Notifications"
+moviesDirectory="/usr/local/screen_recording_tutorial/movies"
 
-## First dialog
-WINDOWTYPE="popup"
-BAR_TITLE="IMPORTANT: Allow your applications to record the screen!"
-TITLE1="Check the box in front of each application in the window that has just opened (videoconference, remote support...). \n"
-BUTTON_1="Next"
+# If the folder that contain the videos is not available, we don't run the script.
 
-PU_PIC="/usr/local/images/screenrecording/screenrecording.png"
 
-RESPONSE=$("${NA_PATH}" \
-    -type "${WINDOWTYPE}" \
-    -bar_title "${BAR_TITLE}" \
-    -title "${TITLE1}" \
-    -main_button_label "${BUTTON_1}" \
-    -accessory_view_type "image" \
-    -accessory_view_payload "${PU_PIC}" \
-    -always_on_top \
-    -silent)
-    
+if [ ! -d "$moviesDirectory" ]; then
+  echo "Le dossier $moviesDirectory est introuvable."
+  exit 1
+fi
+
+#=======================================================================#
+#--------------------#     Language detection      #--------------------#
+#=======================================================================#
+
+langs=$(sudo -u "$consoleUser" defaults read NSGlobalDomain AppleLanguages | sed -n '2p' | cut -c 6-10)
+
+currentlang=$(echo "${langs[1]/,/}") # langs[0] is the open bracket
+
+# echo $currentlang
+echo "$langs"
+
+if [ "$langs" = "fr-FR" ]; then
+  # Chemin du dossier contenant les fichiers vidéo
+  contentFolder="/usr/local/screen_recording_tutorial/movies/fr/"
+else
+  contentFolder="/usr/local/screen_recording_tutorial/movies/en"
+fi
+
+#=======================================================================#
+#--------------------#  SwiftDialog Installation   #--------------------#
+#=======================================================================#
+
+# Requiert l'app SwiftDialog. Elle sera téléchargée et installée si absente.
+
+dialogPath="/usr/local/bin/dialog"
+dialogURL=$(curl --silent --fail "https://api.github.com/repos/bartreardon/swiftDialog/releases/latest" | awk -F '"' "/browser_download_url/ && /pkg\"/ { print \$4; exit }")
+
+if [ ! -e "$dialogPath" ]; then
+  echo "Dialog tool must be installed first."
+  curl -L "$dialogURL" -o "/tmp/dialog.pkg"
+  installer -pkg /tmp/dialog.pkg -target /
+  
+  if [ ! -e "$dialogPath" ]; then
+    echo "An error occured, dialog tool could not be installed."
+    exit 1
+  else
+    echo "Dialog tool available…..."
+  fi
+else 
+  echo "Dialog tool available…"
+fi
+
+
+# Do we open System Preferences or Settings (if macOS > 12)?
+
+sudo -u "$consoleUser" open "x-apple.systempreferences:com.apple.preference.security?Privacy_ScreenCapture"
+
+sleep 2
+
+macosBuild=$(sw_vers -buildVersion | cut -b 1-2)
+
+# macOS builds :
+# Mojave = 18
+# Catalina = 19
+# Big Sur = 20
+# Monterey = 21
+# Ventura = 22
+
+# Function to display the alert using Dialog
+
+displayAlert () {
+  
+  # If contents is not an image, we assume safely that's a video..
+  
+  RESPONSE=$("${dialogPath}" \
+    --title "$dialogTitle" \
+    --videocaption "$dialogMessage" \
+    --button1text "$dialogButton" \
+    --titlefont "colour=#005FAD,size=22" \
+    --position topright \
+    --icon none \
+    --width 48% \
+    --video "$contentFolder/$movieName" --autoplay)
+}
+
+echo "Build de macOS : version $macosBuild"
+
+## First dialog. Different according to macOS version.
+
+if [ "$langs" = "fr-FR" ]; then
+  dialogTitle="IMPORTANT : Autorisez vos applications à enregistrer l'écran !"
+else
+  dialogTitle="IMPORTANT: Allow your applications to record the screen!"
+fi
+
+
+if [ "$langs" = "fr-FR" ] && [ "$macosBuild" -gt "21" ]; then
+  movieName="ScreenRecording_Ventura_Checkbox_FR.mov"
+  dialogMessage="Cochez la case en face de chaque application dans la fenêtre qui vient de s'ouvrir (visioconférence, prise en main à distance…)."
+  dialogButton="Suivant"
+  
+elif [ "$langs" = "fr-FR" ] && [ "$macosBuild" -ge "21" ]; then
+  dialogMessage="Cochez la case en face de chaque application dans la fenêtre qui vient de s'ouvrir (visioconférence, prise en main à distance…)."
+  dialogButton="Suivant"
+  movieName="ScreenRecording_Monterey_Checkbox_FR.mov"
+  
+elif [ "$langs" != "fr-FR" ] && [ "$macosBuild" -gt "21" ]; then
+  dialogMessage="Check the box in front of each application in the window that has just opened (videoconferencing, remote handling...)."
+  dialogButton="Next"
+  movieName="ScreenRecording_Ventura_Checkbox_EN.mov"
+  
+elif [ "$langs" != "fr-FR" ] && [ "$macosBuild" -ge "21" ]; then
+  dialogMessage="Check the box in front of each application in the window that has just opened (videoconferencing, remote handling...)."
+  dialogButton="Next"
+  movieName="ScreenRecording_Monterey_Checkbox_EN.mov"
+fi
+
+displayAlert
+
 ## Second dialog
 
-TITLE="Drag with the trackpad with two fingers down into the window to display more applications to allow. \n"
-PU_PIC="/usr/local/images/screenrecording/screenrecording.gif"
-BUTTON_1="Continuer"
+if [ "$langs" = "fr-FR" ] && [ "$macosBuild" -gt "21" ]; then
+  movieName="ScreenRecording_Ventura_Quit_Reopen_FR.mov"
+  dialogMessage="Si une application affiche un message, cliquez sur Quitter et rouvrir."
+  dialogButton="Suivant"
+  
+elif [ "$langs" = "fr-FR" ] && [ "$macosBuild" -ge "21" ]; then
+  dialogMessage="Glissez avec le trackpad avec deux doigts vers le bas dans la fenêtre pour afficher plus d'applications."
+  movieName="ScreenRecording_Monterey_Check_Trackpad_FR.mov"
+  dialogButton="Continuer"
+  
+elif [ "$langs" != "fr-FR" ] && [ "$macosBuild" -gt "21" ]; then
+  dialogMessage="If an application displays a message, click on Quit and reopen."
+  dialogButton="Next"
+  movieName="ScreenRecording_Ventura_Quit_Reopen_EN.mov"
+  
+elif [ "$langs" != "fr-FR" ] && [ "$macosBuild" -ge "21" ]; then
+  dialogMessage="Swipe up or down on the trackpad with two fingers in the window to display more applications to check."
+  dialogButton="Next"
+  movieName="ScreenRecording_Monterey_Check_Trackpad_EN.mov"
+fi
 
-RESPONSE=$("${NA_PATH}" \
-    -type "${WINDOWTYPE}" \
-    -bar_title "${BAR_TITLE}" \
-    -title "${TITLE}" \
-    -main_button_label "${BUTTON_1}" \
-    -accessory_view_type "image" \
-    -accessory_view_payload "$PU_PIC" \
-    -always_on_top \
-    -silent)
+displayAlert
+
 
 ## Third dialog
 
-TITLE="If you need to allow new applications, click on the menu  > System Preferences and then open Security and Privacy > Privacy.\n"
-PU_PIC="/usr/local/images/screenrecording/prefsysteme.png"
-BUTTON_1="Done !"
+if [ "$langs" = "fr-FR" ] && [ "$macosBuild" -gt "21" ]; then
+  dialogMessage="Pour autoriser de nouvelles applications, cliquez sur le menu  > Réglages Système puis cherchez Enregistrer."
+  movieName="ScreenRecording_Ventura_Search_FR.mov"
+  dialogButton="Quitter"
+  
+elif [ "$langs" = "fr-FR" ] && [ "$macosBuild" -ge "21" ]; then
+  dialogMessage="Pour autoriser de nouvelles applications, cliquez sur le menu  > Préférences Système puis cherchez Enregistrer."
+  movieName="ScreenRecording_Monterey_Search_FR.mov"
+  dialogButton="Quitter"
+  
+elif [ "$langs" != "fr-FR" ] && [ "$macosBuild" -gt "21" ]; then
+  dialogMessage="To authorize new applications, click  menu > System Settings and then search for Screen Record."
+  movieName="ScreenRecording_Ventura_Search_EN.mov"
+  dialogButton="Quit"
+  
+elif [ "$langs" != "fr-FR" ] && [ "$macosBuild" -ge "21" ]; then
+  dialogMessage="To allow new apps, click menu  menu > Security and Privacy, then swipe down and select Screen Recording."
+  movieName="ScreenRecording_Monterey_Search_EN.mov"
+  dialogButton="Quit"
+fi
 
-RESPONSE=$("${NA_PATH}" \
-    -type "${WINDOWTYPE}" \
-    -bar_title "${BAR_TITLE}" \
-    -title "${TITLE}" \
-    -main_button_label "${BUTTON_1}" \
-    -accessory_view_type "image" \
-    -accessory_view_payload "${PU_PIC}" \
-    -always_on_top \
-    -silent)
+displayAlert
 
+# Let's try to quit System Preferences / Settings. Works for any version of macOS, hopefully.
 
-# Quit the System Preferences app
-
-   osascript -e 'tell app "System Preferences" to quit'
-    
+osascript -e 'try' -e 'tell app "System Preferences" to quit' -e 'end try'
